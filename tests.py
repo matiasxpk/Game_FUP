f35bc2b0eac72ff5e6b017d68b42cbc4fc4dc5ea
GuilhermeFA00 <sharkdevv@gmail.com>
2021-07-23 17:37:55 -0300

    Updates!

---
+++ tests.py
@@ -0,0 +1,16 @@
+#LENDO ARQUIVO
+'''handler = open('storyER.txt', 'r')
+    print(handler.read())'''
+
+#The main object
+
+sanityLife = 50
+class Player:
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+priest = Player("Nicodemos", 35 )
